b6bbae96594de85e284819da1f5d5e7d8dd4826f
Chris Zarate <chris@zarate.org>
2012-02-17 00:19:13 -0500

    Add simple bookmarklet.

---
+++ js/sgp.bookmarklet.js
@@ -0,0 +1,24 @@
+/*
+	SuperGenPass bookmarklet
+	Convert to bookmarklet using:
+	http://chris.zarate.org/bookmarkleter
+*/
+
+(function(){
+
+/*
+	Declare localization via query variable, e.g.:	
+		Lang='?en';
+	Available locales: de en es fr hu pt-br zh-hk
+	Default (empty string) provides English (en).
+*/
+
+	Lang='';
+
+	var SGP=document.createElement('script');
+	    SGP.type='text/javascript';
+	    SGP.src='http://mobile.supergenpass.com/js/sgp.js?20120217';
+
+	document.getElementsByTagName('head')[0].appendChild(SGP);
+
+})();

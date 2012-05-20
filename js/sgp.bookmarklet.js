6ecea68d006da305e1353c70b758cdc82406b0f4
Chris Zarate <chris@zarate.org>
2012-05-20 17:24:14 -0400

    Load SGP over HTTPS and stop worrying about what protocol the page is.

--- js/sgp.bookmarklet.js
+++ js/sgp.bookmarklet.js
@@ -15,12 +15,9 @@
 
 	Lang='';
 
-//	Match protocol (http vs. https) to prevent browser complaints
-	Domain=(typeof window.location.protocol!='undefined'&&window.location.protocol=='https:')?'https://d6gnhhjxs73le.cloudfront.net':'http://mobile.supergenpass.com';
-
 	var SGP=document.createElement('script');
 	    SGP.type='text/javascript';
-	    SGP.src=Domain+'/js/sgp.js?20120217';
+	    SGP.src='https://d6gnhhjxs73le.cloudfront.net/js/sgp.js?20120217';
 
 	document.getElementsByTagName('head')[0].appendChild(SGP);
 

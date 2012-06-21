2f5960dda01bd59f200105e8355889274ad2f9df
chriszarate <chris@zarate.org>
2012-06-21 17:02:04 -0300

    Removed protocol from CloudFront link (thanks for the tip).

--- js/sgp.bookmarklet.js
+++ js/sgp.bookmarklet.js
@@ -17,7 +17,7 @@
 
 	var SGP=document.createElement('script');
 	    SGP.type='text/javascript';
-	    SGP.src='https://d6gnhhjxs73le.cloudfront.net/js/sgp.js?20120217';
+	    SGP.src='//d6gnhhjxs73le.cloudfront.net/js/sgp.js?20120217';
 
 	document.getElementsByTagName('head')[0].appendChild(SGP);
 

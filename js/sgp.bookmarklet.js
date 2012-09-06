b24fd0cc8f1af4a8d69b6d115de122d761c8c06a
Chris Zarate <chris@zarate.org>
2012-09-05 22:54:53 -0400

    Updated bookmarklet versioning date.

--- js/sgp.bookmarklet.js
+++ js/sgp.bookmarklet.js
@@ -17,7 +17,7 @@
 
 	var SGP=document.createElement('script');
 	    SGP.type='text/javascript';
-	    SGP.src='//d6gnhhjxs73le.cloudfront.net/js/sgp.js?20120217';
+	    SGP.src='//d6gnhhjxs73le.cloudfront.net/js/sgp.js?20120905';
 
 	document.getElementsByTagName('head')[0].appendChild(SGP);
 

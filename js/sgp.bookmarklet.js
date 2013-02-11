6a2a9fffb7b61b306e9230c48b0a46e814a7aeb2
Chris Zarate <chris@zarate.org>
2013-02-10 20:20:02 -0500

    Add Russian to locale options.

--- js/sgp.bookmarklet.js
+++ js/sgp.bookmarklet.js
@@ -1,5 +1,5 @@
 (function(){
-	Lang='en'; // Available locales: de en es fr hu pt-br zh-hk
+	//Lang='en'; // Available locales: de en es fr hu pt-br ru zh-hk
 	var a=document.createElement('script');
 	a.setAttribute('src','https://mobile.supergenpass.com/sgp.js?20130210');
 	document.getElementsByTagName('head')[0].appendChild(a);

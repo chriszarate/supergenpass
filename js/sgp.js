7e5eadf568d08d90ac60e540b8e49fc751945070
Chris Zarate <czarate@mla.org>
2013-04-26 15:03:56 -0400

    Move variable definitions to top of scope.

--- js/sgp.js
+++ js/sgp.js
@@ -5,8 +5,8 @@
   Adapted from Paul Irish's method: http://pastie.org/462639
 */
 
-  var jQueryURL='//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js';
-  var jQueryMin=1.5;
+  var jQueryURL='//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js',
+      jQueryMin=1.5;
 
   if(typeof $==='undefined'||parseFloat($.fn.jquery)<jQueryMin) {
     var s=document.createElement('script');
@@ -22,52 +22,25 @@
     LoadSGP($);
   }
 
-//  SGP callback wrapper:
-
   function LoadSGP($) {
 
-  //  Generate a random ID for the SGP container <div>.
+    // Variable declarations.
+    var RandID='',
 
-    var RandID='';
-    for(var i=0;i<10;i++) {
-      RandID+='abcdefghijklmnopqrstuvwxyz'.charAt(Math.floor(Math.random()*26));
-    }
+    // Look for declared localization.
+    Query=(typeof Lang==='undefined')?'':'?'+Lang,
 
-  //  Look for declared localization.
-    var Query=(typeof Lang==='undefined')?'':'?'+Lang;
+    // SGP location:
+    Domain='https://mobile.supergenpass.com',
+    FrameURL=Domain+'/'+Query,
 
-  //  SGP location:
-    var FrameURL='https://mobile.supergenpass.com/'+Query;
-    var Domain='https://mobile.supergenpass.com';
+    // Find largest viewport, looping through frames if applicable.
+    $Target=(document)?$(document):false,
+    MaxArea=0,
 
-  //  Find largest viewport, looping through frames if applicable.
-
-    var $Target=(document)?$(document):false;
-    var MaxArea=0;
-
-    $('frame').each(function() {
-      try {
-        var Area=$(this).height()*$(this).width();
-        if(Area>MaxArea) {
-          $Target=$(this.contentWindow.document);
-          MaxArea=Area;
-        }
-      }
-      catch(e) {
-        console.log('SGP: Skipping external frame.');
-      }
-    });
-
-  //  If no target document is found, redirect to mobile version.
-
-    if(!$Target) {
-      window.location=FrameURL;
-    }
-
-  //  Define styles:
-
-    var Styles=
-      '#'+RandID+' {'+
+    // Define styles:
+    Styles=
+      '#% {'+
         'z-index:99999;'+
         'position:absolute;'+
         'top:'+$Target.scrollTop()+'px;'+
@@ -79,7 +52,7 @@
         'border:solid 1px #ddd;'+
         'box-sizing:content-box;'+
       '}'+
-      '#'+RandID+' div {'+
+      '#% div {'+
         'overflow:hidden;'+
         'width:225px;'+
         'margin:0;'+
@@ -94,7 +67,7 @@
         'box-shadow:1px 0px 1px #1a2643,0px 1px 1px #2a3653,2px 1px 1px #1a2643,1px 2px 1px #2a3653,3px 2px 1px #1a2643,2px 3px 1px #2a3653,4px 3px 1px #1a2643,3px 4px 1px #2a3653,5px 4px 1px #1a2643,4px 5px 1px #2a3653,6px 5px 1px #1a2643;'+
         'box-sizing:content-box;'+
       '}'+
-      '#'+RandID+' a {'+
+      '#% a {'+
         'color:#fff !important;'+
         'background:transparent !important;'+
         'font-family:sans-serif !important;'+
@@ -104,24 +77,47 @@
         'border-width:0;'+
         'cursor:pointer;'+
       '}'+
-      '#'+RandID+' iframe {'+
+      '#% iframe {'+
         'position:static;'+
         'width:240px;'+
         'height:184px;'+
         'border:none;'+
         'overflow:hidden;'+
         'pointer-events:auto;'+
-      '}';
+      '}'.replace('#%', RandID),
+
+    // Create SGP elements.
+    $Box=$("<div/>",{id:RandID}),
+    $TitleBar=$("<div/>"),
+    $CloseLink=$("<a/>",{href:'#',text:'x'}),
+    $Frame=$("<iframe/>",{src:FrameURL,scrolling:'no'}),
+    Dragging=null;
+
+    // Generate a random ID for the SGP container <div>.
+    for(var i=0;i<10;i++) {
+      RandID+='abcdefghijklmnopqrstuvwxyz'.charAt(Math.floor(Math.random()*26));
+    }
+
+    $('frame').each(function() {
+      try {
+        var Area=$(this).height()*$(this).width();
+        if(Area>MaxArea) {
+          $Target=$(this.contentWindow.document);
+          MaxArea=Area;
+        }
+      }
+      catch(e) {
+        console.log('SGP: Skipping external frame.');
+      }
+    });
+
+    // If no target document is found, redirect to mobile version.
+    if(!$Target) {
+      window.location=FrameURL;
+    }
 
   //  Append styles to target document.
     $("<style type='text/css'>" + Styles + "</style>").appendTo($('head',$Target));
-
-  //  Create SGP elements.
-    var $Box=$("<div/>",{id:RandID});
-    var $TitleBar=$("<div/>");
-    var $CloseLink=$("<a/>",{href:'#',text:'x'});
-    var $Frame=$("<iframe/>",{src:FrameURL,scrolling:'no'});
-    var Dragging=null;
 
   //  Enable "close window" link.
 

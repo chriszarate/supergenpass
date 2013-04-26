b2c3dfb08ae7cd3c1b1a561b31717fc5105a281b
Chris Zarate <czarate@mla.org>
2013-04-26 16:39:45 -0400

    Move styles inline.

--- js/sgp.js
+++ js/sgp.js
@@ -24,11 +24,8 @@
 
   function LoadSGP($) {
 
-    // Variable declarations.
-    var RandID='',
-
     // Look for declared localization.
-    Query=(typeof Lang==='undefined')?'':'?'+Lang,
+    var Query=(typeof Lang==='undefined')?'':'?'+Lang,
 
     // SGP location:
     Domain='https://mobile.supergenpass.com',
@@ -38,65 +35,17 @@
     $Target=(document)?$(document):false,
     MaxArea=0,
 
-    // Define styles:
-    Styles=
-      '#% {'+
-        'z-index:99999;'+
-        'position:absolute;'+
-        'top:'+$Target.scrollTop()+'px;'+
-        'right:0;'+
-        'width:240px;'+
-        'margin:0;'+
-        'padding:5px;'+
-        'background-color:#fff;'+
-        'border:solid 1px #ddd;'+
-        'box-sizing:content-box;'+
-      '}'+
-      '#% div {'+
-        'overflow:hidden;'+
-        'width:225px;'+
-        'margin:0;'+
-        'padding:5px;'+
-        'color:#fff;'+
-        'background-color:#3a4663 !important;'+
-        'font-size: 1em;'+
-        'text-align:right;'+
-        'text-shadow:1px 1px #0a1633;'+
-        'line-height:10px;'+
-        'cursor:move;'+
-        'box-shadow:1px 0px 1px #1a2643,0px 1px 1px #2a3653,2px 1px 1px #1a2643,1px 2px 1px #2a3653,3px 2px 1px #1a2643,2px 3px 1px #2a3653,4px 3px 1px #1a2643,3px 4px 1px #2a3653,5px 4px 1px #1a2643,4px 5px 1px #2a3653,6px 5px 1px #1a2643;'+
-        'box-sizing:content-box;'+
-      '}'+
-      '#% a {'+
-        'color:#fff !important;'+
-        'background:transparent !important;'+
-        'font-family:sans-serif !important;'+
-        'font-size:13px !important;'+
-        'font-weight:bold !important;'+
-        'text-decoration:none;'+
-        'border-width:0;'+
-        'cursor:pointer;'+
-      '}'+
-      '#% iframe {'+
-        'position:static;'+
-        'width:240px;'+
-        'height:184px;'+
-        'border:none;'+
-        'overflow:hidden;'+
-        'pointer-events:auto;'+
-      '}'.replace('#%', RandID),
+    // Define CSS properties.
+    BoxStyle='z-index:99999;position:absolute;top:'+$Target.scrollTop()+';right:0;width:240px;margin:0;padding:5px;background-color:#fff;border:solid 1px #ddd;box-sizing:content-box;',
+    TitleBarStyle='overflow:hidden;width:235px;height:15px;margin:0;padding:0;background-color:#3a4663;cursor:move;box-shadow:1px 0 1px #1a2643,0 1px 1px #2a3653,2px 1px 1px #1a2643,1px 2px 1px #2a3653,3px 2px 1px #1a2643,2px 3px 1px #2a3653,4px 3px 1px #1a2643,3px 4px 1px #2a3653,5px 4px 1px #1a2643,4px 5px 1px #2a3653,6px 5px 1px #1a2643;box-sizing:content-box;',
+    FrameStyle='position:static;width:240px;height:184px;border:none;overflow:hidden;pointer-events:auto;',
 
     // Create SGP elements.
-    $Box=$("<div/>",{id:RandID}),
-    $TitleBar=$("<div/>"),
-    $CloseLink=$("<a/>",{href:'#',text:'x'}),
-    $Frame=$("<iframe/>",{src:FrameURL,scrolling:'no'}),
+    $Box=$("<div/>",{style:BoxStyle}),
+    $TitleBar=$("<div/>",{style:TitleBarStyle}),
+    $Frame=$("<iframe/>",{src:FrameURL,scrolling:'no',style:FrameStyle}),
+    // Create dragging indicator.
     Dragging=null;
-
-    // Generate a random ID for the SGP container <div>.
-    for(var i=0;i<10;i++) {
-      RandID+='abcdefghijklmnopqrstuvwxyz'.charAt(Math.floor(Math.random()*26));
-    }
 
     $('frame').each(function() {
       try {
@@ -106,9 +55,7 @@
           MaxArea=Area;
         }
       }
-      catch(e) {
-        console.log('SGP: Skipping external frame.');
-      }
+      catch(e){}
     });
 
     // If no target document is found, redirect to mobile version.
@@ -116,18 +63,12 @@
       window.location=FrameURL;
     }
 
-  //  Append styles to target document.
-    $("<style type='text/css'>" + Styles + "</style>").appendTo($('head',$Target));
-
   //  Enable "close window" link.
 
-    $CloseLink.bind('click',function(e) {
-      $Box.remove();
-      e.preventDefault();
-    });
+    $TitleBar.bind('dblclick',function(e){$Frame.toggle();});
 
   //  Append SGP window to target document.
-    $Box.append($TitleBar.append($CloseLink),$Frame).appendTo($('body',$Target));
+    $Box.append($TitleBar,$Frame).appendTo($('body',$Target));
 
   //  Attach postMessage listener to populate password fields and change iframe height.
 

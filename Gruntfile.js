3275adbcbbc0d389ed7fae1a9cf229525523223a
Chris Zarate <chris@zarate.org>
2013-08-25 19:58:25 -0400

    Use Bower for component management.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,22 +7,25 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    meta: {
-      name: 'SuperGenPass',
-      hostedURL: 'https://mobile.supergenpass.com/sgp.js'
-    },
+    pkg: grunt.file.readJSON('package.json'),
 
     jshint: {
       all: ['js/sgp*.js']
     },
 
     uglify: {
+      components: {
+        files: {
+          'build/components.min.js': [
+            'bower_components/jquery/jquery.js',
+            'bower_components/jstorage/jstorage.js',
+            'js/jquery.identicon5.js'
+          ]
+        }
+      },
       app: {
         files: {
-          'build/app.js': [
-            'js/jquery.custom.js',
-            'js/jquery.jstorage.js',
-            'js/jquery.identicon5.js',
+          'build/app.min.js': [
             'js/sgp.hash.js',
             'js/sgp.core.js',
             'js/sgp.form.js'
@@ -31,34 +34,56 @@ module.exports = function(grunt) {
       },
       bookmarklet: {
         files: {
-          'build/sgp.js': ['js/sgp.js'],
-          'build/sgp.bookmarklet.js': ['js/sgp.bookmarklet.js']
+          'build/sgp.js': ['js/sgp.js']
         }
       }
     },
 
-    app: {
-      all: {
+    cssmin: {
+      add_banner: {
+        files: {
+          'build/app.min.css': [
+            'app/app.css'
+          ]
+        }
+      }
+    },
+
+    compile: {
+      app: {
         options: {
-          isTemplate: true,
-          data: {
-            js: 'build/app.js',
-            css: 'css/sgp.css'
+          include: {
+            lib: 'build/components.min.js',
+            js: 'build/app.min.js',
+            css: 'build/app.min.css'
           }
         },
         files: {
-          'build/index.html': ['app/index.html'],
-          'build/sgp.appcache': ['app/sgp.appcache']
+          'build/index.html': ['app/index.html']
+        }
+      },
+      bookmarklet: {
+        options: {
+          bookmarklet: true
         },
+        files: {
+          'build/sgp.bookmarklet.js': ['build/sgp.js']
+        }
       }
     },
 
-    bookmarklet: {
-      all: {
-        options: { isTemplate: true },
-        files: {
-          'build/sgp.bookmarklet.js': ['build/sgp.bookmarklet.js']
-        }
+    manifest: {
+      generate: {
+        options: {
+          basePath: 'build/',
+          network: ['*'],
+          verbose: true,
+          timestamp: true
+        },
+        src: [
+          'index.html'
+        ],
+        dest: 'build/cache.manifest'
       }
     }
 
@@ -67,86 +92,74 @@ module.exports = function(grunt) {
   // Load tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-manifest');
 
-  grunt.registerMultiTask('app', 'Generate self-contained HTML5 app', function() {
+  grunt.registerMultiTask('compile', 'Generate self-contained HTML5 app', function() {
 
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      isTemplate: false
+      bookmarklet: false,
+      include: {},
+    }),
+
+    // File exists helper.
+    fileExists = function(filepath) {
+      if(grunt.file.exists(filepath)) {
+        return true;
+      } else {
+        grunt.log.warn('Source file "' + filepath + '" not found.');
+        return false;
+      }
+    },
+
+    // Read file helper.
+    readFile = function(filepath) {
+      // Read and return the file's source.
+      return grunt.file.read(filepath);
+    };
+
+    // Load include files into include object.
+    Object.keys(options.include).forEach(function(key) {
+      var filepath = options.include[key];
+      if(fileExists(filepath)) {
+        options.include[key] = readFile(filepath)
+      } else {
+        delete options.include[key];
+      }
     });
 
     this.files.forEach(function(file) {
 
-      // From Grunt.js API documentation.
-      var contents = file.src.filter(function(filepath) {
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read and return the file's source.
-        return grunt.file.read(filepath);
-      }).join('');
+      // Load files.
+      var contents = file.src.filter(fileExists).map(readFile).join('');
 
       // Process as template.
-      if(options.isTemplate) contents = grunt.template.process(contents, {data:options.data});
+      contents = grunt.template.process(contents, {data:options.include});
+
+      if(options.bookmarklet) {
+
+        // Wrap in anonymous function.
+        contents = '(function(){' + contents + '})()';
+
+        // Encode as URI.
+        contents = encodeURI('javascript:' + contents);
+
+      }
 
       // Write joined contents to destination filepath.
       grunt.file.write(file.dest, contents);
-      grunt.log.writeln('Created file: ' + file.dest);
-
-    });
-
-    // One-off deletion.
-    grunt.file.delete(options.data.js);
-    grunt.log.writeln('Deleted file: ' + options.data.js);
-
-  });
-
-  grunt.registerMultiTask('bookmarklet', 'Generate bookmarklet', function() {
-
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      anonymize: true,
-      isTemplate: false
-    });
-
-    this.files.forEach(function(file) {
-
-      // From Grunt.js API documentation.
-      var contents = file.src.filter(function(filepath) {
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read and return the file's source.
-        return grunt.file.read(filepath);
-      }).join('');
-
-      // Process script as template.
-      if(options.isTemplate) contents = grunt.template.process(contents);
-
-      // Wrap in anonymous function.
-      if(options.anonymize) contents = '(function(){;' + contents + ';})()';
-
-      // Encode as URI.
-      contents = encodeURI('javascript:' + contents);
-
-      // Write joined contents to destination filepath.
-      grunt.file.write(file.dest, contents);
-      grunt.log.writeln('Created bookmarklet: ' + file.dest);
+      grunt.log.writeln('Compiled file: ' + file.dest);
 
     });
 
   });
 
-  grunt.registerTask('default', ['jshint', 'uglify', 'app', 'bookmarklet']);
-  grunt.registerTask('build-app', ['jshint', 'uglify:app', 'app']);
-  grunt.registerTask('build-bookmarklet', ['jshint', 'uglify:bookmarklet', 'bookmarklet']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'compile']);
+  grunt.registerTask('build', ['jshint', 'uglify:app', 'cssmin', 'compile:app']);
+  grunt.registerTask('bookmarklet', ['jshint', 'uglify:bookmarklet', 'compile:bookmarklet']);
+  grunt.registerTask('components', ['uglify:components']);
+  grunt.registerTask('css', ['cssmin']);
 
 };

2f6f518cf24a9010f3843c4ff093c9f30a340b8c
Chris Zarate <chris@zarate.org>
2013-08-26 01:02:54 -0400

    Remove hosted bookmarklet.

--- js/sgp.bookmarklet.js
+++
@@ -1,3 +0,0 @@
-var a=document.createElement('script');
-a.setAttribute('src','<%= meta.hostedURL %>?<%= grunt.template.today(\'yyyymmdd\') %>');
-document.getElementsByTagName('head')[0].appendChild(a);

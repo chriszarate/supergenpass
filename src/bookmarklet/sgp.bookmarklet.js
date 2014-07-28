cc07e85d672d0cba70c8be3f360b84cdfd4fa705
chriszarate <chris@zarate.org>
2014-07-27 23:47:43 -0400

    Better error trapping and fallbacks to mobile, driven by @cmcnulty. Fixes #46 and #48.

--- src/bookmarklet/sgp.bookmarklet.js
+++ src/bookmarklet/sgp.bookmarklet.js
@@ -5,7 +5,19 @@
   var domain = 'https://chriszarate.github.io';
   var mobile = 'https://chriszarate.github.io/supergenpass/mobile/';
   var minFrameArea = 100000;
-  var loadedSGP = false;
+  var loadTimeout = 2; // seconds
+
+  // Messages
+  var securityMessage = 'SGP may be blocked on this site by a security setting.';
+  var offerQuestion = 'Would you like to open the mobile version?';
+  var offerLink = 'You may wish to <a href="' + mobile + '" target="_blank">open the mobile version</a>.';
+
+  var offerMobileVersion = function () {
+    var redirect = confirm(securityMessage + ' ' + offerQuestion);
+    if (redirect) {
+      window.open(mobile);
+    }
+  };
 
   // Main
   var loadSGP = function($) {
@@ -54,7 +66,66 @@
       catch(e) {}
     };
 
-    var closeWindow = function() {
+    var removeLoadingIndicator = function () {
+      $loadingIndicator.remove();
+    };
+
+    var linkMobileVersion = function () {
+      $loadingIndicator.html(securityMessage + ' ' + offerLink);
+    };
+
+    var postMessage = function () {
+      // Send current bookmarklet version to SGP generator. (Also communicates
+      // current URL and opens channel for response.)
+      try {
+        this.contentWindow.postMessage('{"version":' + version + '}', domain);
+      } catch(e) {
+        linkMobileVersion();
+      }
+    };
+
+    var receiveMessage = function (e) {
+      var post = e.originalEvent;
+      if(post.origin === domain && typeof post.data !== 'undefined') {
+        removeLoadingIndicator();
+        clearTimeout(loadTimeoutID);
+        processMessage(JSON.parse(post.data));
+      }
+    };
+
+    var processMessage = function (data) {
+      $.each(data, function (key, value) {
+        switch(key) {
+          case 'result':
+            populatePassword(value);
+            break;
+          case 'height':
+            changeFrameHeight(Math.max(parseInt(value, 10), 167) + 2);
+            break;
+        }
+      });
+    };
+
+    var populatePassword = function (password) {
+      // Populate generated password into password fields.
+      $('input:password:visible', $localFrames)
+        .css('background', '#9f9')
+        .val(password)
+        .trigger('change click')
+        .on('input', resetPasswordField)
+        .focus();
+    };
+
+    var changeFrameHeight = function (height) {
+      // Change iframe height to match SGP generator document height.
+      $frame.css('height', height);
+    };
+
+    var resetPasswordField = function () {
+      $(this).css('background', '#fff');
+    };
+
+    var closeWindow = function () {
       $box.remove();
     };
 
@@ -62,22 +133,27 @@
     var boxStyle = 'z-index:99999;position:absolute;top:0;right:5px;width:258px;margin:0;padding:0;box-sizing:content-box;';
     var titleBarStyle = 'overflow:hidden;width:258px;height:20px;margin:0;padding:0;text-align:right;background-color:#356;cursor:move;box-sizing:content-box;';
     var closeLinkStyle = 'padding:0 5px;color:#fff;font-size:18px;line-height:20px;cursor:pointer;';
+    var loadingIndicatorStyle = 'position:absolute;width:258px;height:190px;padding:15px;color:#333;background-color:#fff;font-family:monospace;font-size:15px;text-align:center;';
     var frameStyle = 'position:static;width:258px;height:190px;border:none;overflow:hidden;pointer-events:auto;';
 
     // Create SGP elements.
     var $box = $('<div/>', {style: boxStyle});
     var $titleBar = $('<div/>', {style: titleBarStyle});
     var $closeLink = $('<span/>', {style: closeLinkStyle}).append('×');
+    var $loadingIndicator = $('<div/>', {style: loadingIndicatorStyle}).append('Loading SGP ...');
     var $frame = $('<iframe/>', {src: mobile, scrolling: 'no', style: frameStyle});
 
     // Find largest viewport, looping through frames if applicable.
     $('frame').filter(isLocalFrame).each(findBiggestFrame);
     $('iframe', $target).filter(isLocalFrame).each(findBiggestFrame);
 
-    // If no target document is found, redirect to mobile version.
+    // If no target document is found, offer mobile version.
     if(!$target) {
-      window.location = mobile;
+      offerMobileVersion();
     }
+
+    // If SGP doesn't load after a timeout, offer mobile version.
+    var loadTimeoutID = setTimeout(linkMobileVersion, loadTimeout * 1000);
 
     // Provide "close window" feature.
     $closeLink.on('click', closeWindow);
@@ -89,42 +165,13 @@
     // Blur any active form fields.
     $(document.activeElement).blur();
 
+    // Attach postMessage listener and responder.
+    $frame.on('load', postMessage);
+    $(window).on('message', receiveMessage);
+
     // Append SGP window to target document.
     $titleBar.append($closeLink);
-    $box.append($titleBar, $frame).appendTo($('body', $target));
-
-    // Attach postMessage listener for responses from SGP generator.
-    $(window).on('message', function (e) {
-      var post = e.originalEvent;
-      if(post.origin === domain && typeof post.data !== 'undefined') {
-        $.each(JSON.parse(post.data), function (key, value) {
-          switch(key) {
-            // Populate generated password into password fields.
-            case 'result':
-              $('input:password:visible', $localFrames)
-                .css('background', '#9f9')
-                .val(value)
-                .trigger('change click')
-                .on('input', function () {
-                  $(this).css('background', '#fff');
-                })
-                .focus();
-              break;
-            // Change iframe height to match SGP generator document height.
-            case 'height':
-              $frame.css('height', Math.max(parseInt(value, 10), 167) + 2);
-              break;
-          }
-        });
-      }
-    });
-
-    // Send current bookmarklet version to SGP generator. (Also communicates
-    // current URL and opens channel for response.)
-    $frame.on('load', function () {
-      this.contentWindow.postMessage('{"version":' + version + '}', domain);
-      loadedSGP = true;
-    });
+    $box.append($titleBar, $loadingIndicator, $frame).appendTo($('body', $target));
 
     /*
       Start drag listener.
@@ -154,8 +201,6 @@
       }
     });
 
-    return true;
-
   };
 
   /*
@@ -164,9 +209,11 @@
     http://pastie.org/462639
   */
 
-  var hasJQuery = $ && $.fn && parseFloat($.fn.jquery) >= 1.7 && loadSGP($);
+  if($ && $.fn && parseFloat($.fn.jquery) >= 1.7) {
 
-  if(!hasJQuery) {
+    loadSGP($);
+
+  } else {
 
     var s = document.createElement('script');
     s.src = '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
@@ -176,17 +223,11 @@
         loadSGP(jQuery.noConflict());
       }
     };
+    s.addEventListener('error', offerMobileVersion);
+    s.addEventListener('abort', offerMobileVersion);
 
     document.getElementsByTagName('head')[0].appendChild(s);
 
   }
-
-  // Set timeout to see if SGP has loaded; otherwise assume that loading was
-  // blocked by an origin policy or other content security setting.
-  setTimeout(function() {
-    if(!loadedSGP) {
-      window.location = mobile;
-    }
-  }, 2000);
 
 })(window.jQuery);

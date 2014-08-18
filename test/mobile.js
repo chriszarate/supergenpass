96d288e9fc25d2ea5a5b691ea55f4d9ec06c1bb7
chriszarate <chris@zarate.org>
2014-08-18 00:54:49 -0400

    Make tests compatible with async.

--- test/mobile.js
+++ test/mobile.js
@@ -17,84 +17,99 @@ var selectors =
     'Output'
   ];
 
+// Create click event.
+var clickEvent = document.createEvent('Event');
+clickEvent.initEvent('click', true, true);
 
-var testMobileVersion = function () {
+// Populate selector cache.
+$.each(selectors, function (i, val) {
+  $el[val] = $('#' + val, $iframe[0].contentWindow.document);
+});
 
-  // Populate selector cache.
-  $.each(selectors, function (i, val) {
-    $el[val] = $('#' + val, $iframe[0].contentWindow.document);
-  });
-
-  // Create click event.
-  var clickEvent = document.createEvent('Event');
-  clickEvent.initEvent('click', true, true);
-
-  test('Password generation', function () {
-
-    expect(6);
-
-    // Set initial form values.
-    $el.Passwd.val('test');
-    $el.Secret.val('secret');
-    $el.Domain.val('https://login.example.com');
-    $el.Len.val('10');
-    $el.MethodMD5.prop('checked', true);
-    $el.DisableTLD.prop('checked', false);
-
-    // Send click event and test output.
-    $el.Generate[0].dispatchEvent(clickEvent);
-    ok($el.Output.text() === 'iTbF7RViG5', 'Generated "iTbF7RViG5".');
-
-    // Clear secret password.
-    $el.Secret.val('');
-
-    // Send click event and test output.
-    $el.Generate[0].dispatchEvent(clickEvent);
-    ok($el.Output.text() === 'w9UbG0NEk7', 'Generated "w9UbG0NEk7".');
-
-    // Change hash method to SHA-512.
-    $el.MethodSHA512.prop('checked', true);
-
-    // Send click event and test output.
-    $el.Generate[0].dispatchEvent(clickEvent);
-    ok($el.Output.text() === 'sJfoZg3nU8', 'Generated "sJfoZg3nU8".');
-
-    // Change length to 2 (test input validation).
-    $el.Len.val('2');
-
-    // Send click event and test output.
-    $el.Generate[0].dispatchEvent(clickEvent);
-    ok($el.Output.text() === 'aC81', 'Generated "aC81".');
-
-    // Change length to 100 (test input validation) and add secret password.
-    $el.Len.val('100');
-    $el.Secret.val('ssshh');
-
-    // Send click event and test output.
-    $el.Generate[0].dispatchEvent(clickEvent);
-    ok($el.Output.text() === 'fd35Ng0Xwne2Pb8f3XFu8r8y', 'Generated "fd35Ng0Xwne2Pb8f3XFu8r8y".');
-
-    // Disable subdomain removal.
-    $el.Domain.val('https://login.example.com');
-    $el.DisableTLD.prop('checked', true);
-
-    // Send click event and test output.
-    $el.Generate[0].dispatchEvent(clickEvent);
-    ok($el.Output.text() === 'alrcP2cLv1lDddHXjExlS0H9', 'Generated "alrcP2cLv1lDddHXjExlS0H9".');
-
-  });
-
-  test('Local storage', function () {
-
-    expect(4);
-    ok(localStorage.getItem('Len') === '24', 'Password length value stored.');
-    ok(localStorage.getItem('Salt') === 'ssshh', 'Secret password value stored.');
-    ok(localStorage.getItem('Method') === 'sha512', 'Hash method setting stored.');
-    ok(localStorage.getItem('DisableTLD') === 'true', 'Subdomain removal setting stored.');
-
-  });
-
+// Send click event.
+var sendClick = function () {
+  $el.Generate[0].dispatchEvent(clickEvent);
 };
 
-// Load tests.
-testMobileVersion();
+
+/* Test setup */
+
+var setupTest1 = function () {
+  $el.Passwd.val('test');
+  $el.Secret.val('secret');
+  $el.Domain.val('https://login.example.com');
+  $el.Len.val('10');
+  $el.MethodMD5.prop('checked', true);
+  $el.DisableTLD.prop('checked', false);
+};
+
+var setupTest2 = function () {
+  $el.Secret.val('');
+};
+
+var setupTest3 = function () {
+  $el.MethodSHA512.prop('checked', true);
+};
+
+var setupTest4 = function () {
+  $el.Len.val('2');
+};
+
+var setupTest5 = function () {
+  $el.Len.val('100');
+  $el.Secret.val('ssshh');
+};
+
+var setupTest6 = function () {
+  $el.Domain.val('https://login.example.com');
+  $el.DisableTLD.prop('checked', true);
+};
+
+
+/* Test data */
+
+var testData = [
+  [setupTest1, 'iTbF7RViG5'],
+  [setupTest2, 'w9UbG0NEk7'],
+  [setupTest3, 'sJfoZg3nU8'],
+  [setupTest4, 'aC81'],
+  [setupTest5, 'fd35Ng0Xwne2Pb8f3XFu8r8y'],
+  [setupTest6, 'alrcP2cLv1lDddHXjExlS0H9']
+];
+
+var testLength = testData.length;
+var testIndex = 0;
+
+
+/* Chain tests */
+
+var nextTest = function () {
+  if (testIndex < testLength) {
+    testData[testIndex][0].call();
+    runTest();
+  } else if (testIndex === testLength) {
+    testLocalStorage();
+    start();
+  }
+};
+
+var runTest = function () {
+  sendClick();
+  setTimeout(function () {
+    ok($el.Output.text() === testData[testIndex][1], 'Generated "' + testData[testIndex][1] + '".');
+    testIndex++;
+    nextTest();
+  }, 100);
+};
+
+var testLocalStorage = function () {
+  ok(localStorage.getItem('Len') === '24', 'Password length value stored.');
+  ok(localStorage.getItem('Salt') === 'ssshh', 'Secret password value stored.');
+  ok(localStorage.getItem('Method') === 'sha512', 'Hash method setting stored.');
+  ok(localStorage.getItem('DisableTLD') === 'true', 'Subdomain removal setting stored.');
+};
+
+asyncTest('Password generation', function () {
+  expect(10);
+  nextTest();
+});
